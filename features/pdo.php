2ec4669a58dc6ef8a276710af5b1ccb941e6f442
solenehuault <solene.huault@gmail.com>
2016-09-20 09:50:00 +0200

    renomage de fichiers

--- features/pdo.php
+++
@@ -1,6 +0,0 @@
-<?php
-   /* class user {
-        private $id;
-        private $pseudo;
-    }*/
-?>
--- features/user.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class user {
-        private $id;
-        private $pseudo;
-        private $mail;
-        private $password;
-        private $key;
-    }
-?>
