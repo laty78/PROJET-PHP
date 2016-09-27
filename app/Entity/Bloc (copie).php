639b74efbaf017b709d6e672c81fce9e6c41d390
RonaldM- <ronald.marcel.mr@gmail.com>
2016-09-27 15:05:57 +0200

    Backup Class Bloc

---
+++ app/Entity/Bloc (copie).php
@@ -0,0 +1,163 @@
+<?php
+namespace Entity;
+
+class Bloc {
+    private $title;
+    private $id;
+    private $date;
+    /*private $media;*/
+    private $media_image;
+    private $media_video;
+    private $media_audio;
+    private $format;
+    
+    
+    public function setTitle($title){
+        $this->title = $title;
+    }
+
+    /*public function setMedia($media){
+        $this->media = $media;
+    }*/
+
+    public function setMediaImage($image){
+        $this->media_image = $image;
+    }
+    public function setMediaVideo($video){
+        $this->media_video = $video;
+    }
+    public function setMediaAudio($audio){
+        $this->media_audio = $audio;
+    }
+    public function setDate($date){
+        $this->date = $date;
+    }
+    public function setFormat($format){
+        if(!is_numeric($format)){
+            $format=intval($format);
+        }
+        $this->format = $format;
+    }
+    public function create(){
+        $db = DBSingleton::getInstance();
+
+        /* INSERTION GENERIQUE */
+        $params = array();
+        $entree = "title, date, media_image, media_video, media_audio, format";
+        /*$valeur_entree= array("NULL");*/
+        if(null!=($this->title)) {
+            $titre= $this->title;
+            /*array_push($entree, "title");*/
+            array_push($params, $titre);
+            /*array_push($valeur_entree, "?");*/
+        }
+        if(is_null($this->date)){
+            $day= $this->date = date("Y-m-d");
+            /*array_push($entree, "date");*/
+            array_push($params, $day);
+            /*array_push($valeur_entree, "?");*/
+        }
+        if(null!=($this->media_image)) {
+            $imag= $this->media_image;
+            /*array_push($entree, "media_image");*/
+            array_push($params, $imag);
+            /*array_push($valeur_entree, "?");*/
+        }
+        if(null==($this->media_image)){
+            array_push($params,"null");
+        }
+        if(null!=($this->media_video)) {
+            $vid= $this->media_video;
+            /*array_push($entree, "media_video");*/
+            array_push($params, $vid);
+            /*array_push($valeur_entree, "?");*/
+        }
+        if(null==($this->media_video)){
+            array_push($params,"null");
+        }
+        if(null!=($this->media_audio)) {
+            $aud= $this->media_audio;
+            echo $this->media_audio;
+            /*array_push($entree, "media_audio");*/
+            array_push($params, $aud);
+            /*array_push($valeur_entree, "?");*/
+        }
+        if(null==($this->media_audio)){
+            array_push($params,"null");
+        }
+        if(null!=($this->format)) {
+            $form= $this->format;
+            /*array_push($entree, "format");*/
+            array_push($params, $form);
+            /*array_push($valeur_entree, "?");*/
+        }
+        //echo $valeur_entree;
+        /*CONVERTIR UN TABLEAU EN STRING*/
+        print_r($params);
+        /*$str = implode(",", $entree);*/
+        /*$str_val = implode(",", $valeur_entree);*/
+        /*echo $str;*/
+            $sq= "INSERT INTO bloc (id,";
+            /*$entree= "title, date, media., format";*/
+            $valeur_entree= "NULL,?,?,?,?,?,?";
+
+
+            /*$tab= array($this->title, $this->date, $this->media_image, $this->media_video, $this->media_audio, $this->format);*/
+            $sql= $sq.''.$entree.') VALUES ('.$valeur_entree.')';
+            /* case"media_image":*/
+            /*    $sql = "INSERT INTO bloc (id, title, date, media_image, format) VALUES (NULL, ?, ?, ?, ?);";*/
+                /*break;
+            case"media_image && media_audio":
+                $sql = "INSERT INTO bloc (id, title, date, media_image, media_audio, format) VALUES (NULL, ?, ?, ?, ?, ?);";
+                break;
+            case"media_video":
+                $sql = "INSERT INTO bloc (id, title, date, media_video, format) VALUES (NULL, ?, ?, ?, ?);";
+                break;
+            case"media_audio":
+                $sql = "INSERT INTO bloc (id, title, date, media_audio, format) VALUES (NULL, ?, ?, ?, ?);";
+                break;*/
+        echo $sql;
+        print_r($params);
+        $statement = $db->prepare($sql);
+        $statement->execute($params);
+        /*Pour afficher les errors sql*/
+        print_r($statement->errorInfo());
+    }
+    public function select(){
+        $db = DBSingleton::getInstance();
+        $sq = "SELECT * FROM bloc WHERE title =";
+        $string_title= $this->title;
+        $sql= $sq.'\''.$string_title.'\'';
+        echo $sql;
+        $pdostatement = $db->prepare($sql);
+        $pdostatement->execute(array($this->title));
+        //FETCH_CLASS pour retouner proprement sans le jeu de résultat.
+        $tableau = $pdostatement->fetchAll(\PDO::FETCH_BOTH);
+        $counter=0;
+        print_r($pdostatement);
+        /*print_r($tableau);*/
+        /*POUR DEBUG
+        foreach ($tableau as $row) {
+            $bloc_title = $row["title"];
+            if ($bloc_title == "Test") {
+                $counter +=1;
+                echo "Le post existe bien";
+                echo $bloc_title;
+            } else {
+                echo "Le post est créer";
+            }
+        }*/
+        foreach($tableau as $row) {
+            $bdtitle = $row['title'];
+        }
+        if($bdtitle = ($string_title)){
+            echo 'Le post "'. $this->title . '" est dejà présent';
+        } 
+        else {
+            echo 'Post présent';
+        }
+    }
+
+}
+
+?>
